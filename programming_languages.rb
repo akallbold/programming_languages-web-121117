82820a3c34eb92ac7d4798e03998f5b19b18283a
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-26 20:28:05 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,48 @@
+# require 'pry'
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+    languages.each do |style,data|
+      data.each do |language,type_value|
+        type_value.each do |type_key,type_desc|
+          new_hash[language] = {type: type_desc, style: [].push(style) }
+          # binding.pry
+        end
+      end
+    end
+  new_hash
 end
+
+# reformat_languages(languages)
